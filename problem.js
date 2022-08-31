23dc6fcf266cb297669f53c20017f9868bbbfe74
Mahinkahn <mahinkahn37@gmail.com>
2022-08-31 12:33:26 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,21 @@
+// problem 1
+var fruits = ["Apple", "Banana", "Orange"];
+
+// find index of Banana
+var bananaIndex = fruits.indexOf("Banana");
+console.log(bananaIndex);
+
+//Replace Bannan with Mango
+fruits[1] = "Mango";
+console.log(fruits);
+
+// remove Orange 
+fruits.pop();
+console.log(fruits);
+
+// add watermelon 
+fruits.push("watermelon")
+console.log(fruits);
+
+// problem 1 end
+
---
+++ problem2.js
@@ -0,0 +1,19 @@
+// problem 2
+
+var score = 39;
+
+if (score >= 80) {
+    console.log('A Grade');
+}
+else if (score >= 60) {
+    console.log('B Grade');
+}
+else if (score >= 50) {
+    console.log('D Grade');
+}
+else if (score >= 40) {
+    console.log('D Grade');
+}
+else if (score >= 39) {
+    console.log('f Grade');
+}
